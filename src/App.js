c5f8265fe1be01be2204b75809178e4bc616f009
SC3 <isc1835051@gmail.com>
2022-05-26 10:05:05 -0500

    avance

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const tipos = {
+  artifacts:"artefactos",
+  boss:"jefes",
+  characters:"personajes",
+  consumables:"consumibles",
+  domains:"dominios",
+  elements:"elementos",
+  enemies:"enemigos",
+  materials:"materiales",
+  nations:"naciones",
+  weapons:"armas",
+};
 
 function App() {
+
+  const [genshinState, setGenshinState] =useState ({
+    types :[],
+  });
+
+  const fetchGenshinApi = async (url="https://api.genshin.dev/") => {
+    const respuesta = await fetch(url);
+
+    const respJson = await respuesta.json();
+    if(item === "types"){
+      setGenshinState({
+        types:respJson.types,
+      });
+    }
+    
+setGenshinState({
+types,
+});
+  };
+
+  fetchGenshinApi();
+
+  const handleChangeType =({target}) =>{
+const url =
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>GENSHIN IMPACT DEX</h1>
+      <select name="types" onChange={handleChangeType}>
+<option value= "">Seleccione una opcion</option>
+{genshinState.types.map((type) => (
+
+<option key={type} value={type}>
+{tipos[type]}
+
+</option>
+
+  ))}
+      </select>
+ 
     </div>
   );
 }
