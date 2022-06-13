4f3e395279b0e94956387e4d8ecbbd92d0b51c7a
SC3 <isc1835051@gmail.com>
2022-06-13 10:03:47 -0500

    artefactos

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const tipos = {
+  artifacts: "Artefactos",
+  boss: "Jefes",
+  characters:"Personajes",
+  consumables: "Consumibles",
+  domains:"Dominios",
+  elements:"Elementos",
+  enemies: "Enemigos",
+  materials:"Materiales",
+  nations:"Naciones",
+  weapons:"Armas",
+};
+
+function App() {
+  const [genshinState, setGenshinState] = useState({
+    types: []
+  });
+
+  const fetchGenshinApi = async (item, url = "https://api.genshin.dev/") => {
+    const respuesta = await fetch(url);
+    const respJSON = await respuesta.json();
+
+    if (item==="types"){
+      setGenshinState({
+        ...genshinState,
+        types:respJSON.types,
+      });      
+    }else{
+      setGenshinState({
+        ...genshinState,
+        [item]:respJSON,
+      });
+    }
+console.log(respJSON);
+  
+  };
+
+  useEffect(() => {
+fetchGenshinApi("types");
+  }, []);
+
+
+  const handleChangeType = ({ target }) => {
+    const url = `https://api.genshin.dev/${target.value}`;
+    fetchGenshinApi(target.value, url);
+    console.log(genshinState);
+  };
+
+  return (
+    <div className="App container">
+      <h1>Genshin Impact Dex</h1>
+      <hr />
+      <select name="types" onChange={handleChangeType} >
+        <option value=""> Seleccione un elemento</option>
+        {genshinState.types.map((type) => ( 
+          <option key={type} value={type} >
+            {tipos[type]}
+            </option>
+        )
+        )}
+        
+
+      </select>
+      {genshinState.artifacts &&(
+        <select name="artifacts">
+          <option value="">Seleccione una opcion</option>
+          {genshinState.artifacts.map((artifact)=> (
+            <option key ={artifact} value={artifact}>
+              {artifact}
+            </option>
+          ))}
+        </select>
+      )}
+
+    </div>
+  );
+}
+
+export default App;
